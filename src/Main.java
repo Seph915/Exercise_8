070e8aa160d1b07c116ce6a132a594de90b7dad5
fabio <fabiocostanza91@gmail.com>
2023-07-18 11:56:50 +0200

    exercise 8

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String MyName= "fabio";
+        int nameLength = MyName.length();
+        if (nameLength % 2 == 0) {
+            System.out.println("The numbers of letters in my name is even");
+        }else {
+            System.out.println("The numbers of letters in my name is odd");
+        }
+    }
+}
